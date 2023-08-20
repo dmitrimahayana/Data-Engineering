9539097b10f7d7a2463cd6cd5601bd2bc0437b74
dmitrimahayana <dmitri.mahayana@gmail.com>
2023-08-20 15:06:08 +0700

    update cli

--- Run Local_Scala_IDX_CreateModel.bat
+++ Run Local_Scala_IDX_CreateModel.bat
@@ -1,1 +1,1 @@
-spark-submit --master spark://192.168.1.7:7077 --packages org.mongodb.spark:mongo-spark-connector_2.12:10.2.0 Jars\Scala-IDX-Stock-Analysis-CreateModel\Scala-IDX-Stock-Analysis.jar
+spark-submit --master spark://192.168.1.7:7077 --class CreateModel --packages org.mongodb.spark:mongo-spark-connector_2.12:10.2.0 Jars\Scala-IDX-Stock-Analysis-CreateModel\Scala-IDX-Stock-Analysis.jar
--- Start Local Spark Worker.bat
+++ Start Local Spark Worker.bat
@@ -1,1 +1,2 @@
-spark-class org.apache.spark.deploy.worker.Worker spark://192.168.1.7:7077 -m 6g -c 3
+for /f "delims=[] tokens=2" %%a in ('ping -4 -n 1 %ComputerName% ^| findstr [') do set NetworkIP=%%a
+spark-class org.apache.spark.deploy.worker.Worker spark://%NetworkIP%:7077 -m 6g -c 3
