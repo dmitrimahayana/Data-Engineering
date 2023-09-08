c82b826fa529f1e4afc1a95f2f3504701b7b855a
dmitrimahayana <dmitri.mahayana@gmail.com>
2023-09-08 10:29:37 +0700

    add playwright java

---
+++ solution/src/main/java/org/solution/DispatcherCollection.java
@@ -0,0 +1,13 @@
+package org.solution;
+
+public class DispatcherCollection {
+    public Integer id;
+    public String fullText;
+    public String selector;
+
+    public DispatcherCollection(Integer id, String fullText, String selector){
+        this.id = id;
+        this.fullText = fullText;
+        this.selector = selector;
+    }
+}
