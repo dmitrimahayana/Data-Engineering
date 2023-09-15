3a4789a681625e1992727f2495884102626bc53f
dmitrimahayana <dmitri.mahayana@gmail.com>
2023-09-15 19:46:00 +0700

    updaate airflow postgre to bigquery

--- airflow/dags/GCP_Digimon_ETL.py
+++ airflow/dags/GCP_Digimon_ETL.py
@@ -9,11 +9,10 @@ from airflow.operators.python_operator import PythonOperator
 from airflow.providers.google.cloud.operators.bigquery import BigQueryInsertJobOperator
 from airflow.providers.google.cloud.operators.bigquery import BigQueryDeleteTableOperator
 from airflow.contrib.operators.bigquery_operator import BigQueryOperator
-from datetime import datetime
 
 # Define the default arguments for the DAG
 default_args = {
-    'owner': 'your_name',
+    'owner': 'Dmitri',
     'start_date': datetime(2023, 9, 12),
     'retries': 1,  # Number of retries if a task fails
     'retry_delay': timedelta(minutes=5),  # Time between retries
@@ -21,9 +20,9 @@ default_args = {
 
 # Create a DAG instance
 dag = DAG(
-    'GCP_BigQuery_ETL', 
+    'BigQuery_Digimon_ETL', 
     default_args=default_args,
-    description='A simple Airflow DAG with two tasks',
+    description='A simple Airflow DAG to Load Digimon Dataset to BigQuery',
     schedule_interval=None,  # Set the schedule interval (e.g., None for manual runs)
     catchup=False  # Do not backfill (run past dates) when starting the DAG
 )
@@ -50,7 +49,7 @@ delete_table = BigQueryDeleteTableOperator(
 ## Function ETL using Pandas GBQ
 def etl_function():
     # Extract data into a DataFrame (example: CSV file)
-    df = pd.read_csv('/opt/airflow/dags/Digimon_Dataset/DigiDB_digimonlist.csv')
+    df = pd.read_csv('/opt/airflow/dataset/Digimon_Dataset/DigiDB_digimonlist.csv')
     
     # Perform transformations (example: converting column names to lowercase)
     df.columns = df.columns.str.lower()
